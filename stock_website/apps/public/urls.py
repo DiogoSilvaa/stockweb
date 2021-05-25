dd2f67c876fe5aa237ecf5fa652535e710054242
DiogoSilva <diogonnobrega@gmail.com>
2021-05-25 15:13:55 +0100

    deleted cache files

--- stock_website/apps/public/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, stock_data
-
-app_name = "public"
-
-urlpatterns = [
-    path('api/data', stock_data.as_view()),
-    path('', index, name="index")
-]
