0d5b433b43f8527f1b171def554335b9182f3821
DiogoSilva <diogonnobrega@gmail.com>
2021-05-27 13:38:09 +0100

    Graphs working based on 100d

--- stock_website/apps/public/views.py
+++ stock_website/apps/public/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html')
+
